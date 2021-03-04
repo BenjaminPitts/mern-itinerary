12c9cfa1829afe2741ef386e0ee7d95674ec9e72
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-03-04 12:06:08 -0700

    full crud

--- public/app.js
+++ public/app.js
@@ -50,60 +50,44 @@ componentDidMount=()=>{
     })
   })
 }
-showStats=(event)=>{
-  event.preventDefault()
-  let stats = this.state.showStats
-  axios.get('/animal_shelter/' + event.target.id).then((response)=>{
 
-      if(stats){
-        this.setState({
-          showStats:false,
-          animals: response.data
-        })
-      }else{
-        this.setState({
-          showStats:true,
-          animals: response.data
-        })
-    }
-  })
-}
 
 render=()=>{
   return (
 <div className='main'>
-  <h1>Animal Shelter</h1>
-  <h2>Create New Post:</h2>
-<form onSubmit={this.submit}>
+<div className='header'>
+  <h1>Disney Itinerary</h1>
+  <h2>Create New Adventure:</h2>
+    <form onSubmit={this.submit}>
   <label htmlFor='name'>Name:</label>
-<input type='text' id='name' value={this.state.name} onChange={this.change} />
+  <input type='text' id='name' value={this.state.name} onChange={this.change} />
   <br />
-  <label htmlFor='breed'>Breed:</label>
-<input type='text' id='breed' value={this.state.breed} onChange={this.change} />
+  <label htmlFor='breed'>Activity1:</label>
+  <input type='text' id='breed' value={this.state.breed} onChange={this.change} />
   <br />
-  <label htmlFor='gender'>Gender:</label>
-<input type='text' id='gender' value={this.state.gender} onChange={this.change} />
+  <label htmlFor='gender'>Activity2:</label>
+  <input type='text' id='gender' value={this.state.gender} onChange={this.change} />
   <br />
-  <label htmlFor='image'>Image:</label>
-<input type='text' id='image' value={this.state.image} onChange={this.change} />
+  <label htmlFor='image'>Image URL:</label>
+  <input type='text' id='image' value={this.state.image} onChange={this.change} />
   <br />
-<input type='submit' value='Create New Post' />
+  <input type='submit' value='Create' />
   <br />
-</form>
+    </form>
+</div>
   <br />
-
-  <h2>Please Adopt Me!</h2>
-  <br />
-  <button value={this.state._id} onClick={this.showStats}>Show Stats</button>
   <br />
   <div className='mainCage'>
   {this.state.animals.map((animal, i)=>{
     return (
       <div className='cage' key={i}>
-      <h4>Name: {animal.name} </h4>
-      <h4>{ this.state.showStats ? 'Breed: ' + animal.breed : null }</h4>
-      <h4>{ this.state.showStats ? 'Gender: ' + animal.gender : null }</h4>
-      <img src={animal.image} alt={animal.name} />
+      <details>
+      <summary>{animal.name}'s Itinerary</summary>
+      <ul>
+        <img src={animal.image} />
+        <li>Activity1: {animal.breed}</li>
+        <li>Activity2: {animal.gender}</li>
+      </ul>
       <br />
       <details>
         <summary>Edit this Post:</summary>
@@ -111,19 +95,22 @@ render=()=>{
           <label htmlFor='name'>Name:</label>
             <input type='text' id='name' value={animal.name} onChange={this.change} />
             <br />
-            <label htmlFor='name'>Breed:</label>
+            <label htmlFor='name'>Activity1: </label>
             <input type='text' id='breed' onChange={this.change} />
             <br />
-            <label htmlFor='name'>Gender:</label>
+            <label htmlFor='name'>Activity2: </label>
             <input type='text' id='gender' onChange={this.change} />
             <br />
-            <label htmlFor='name'>Image:</label>
+            <label htmlFor='name'>Image URL:</label>
             <input type='text' id='image' onChange={this.change} />
             <br />
             <input type='submit' value='Update Post' />
         </form>
       </details>
       <button value={animal._id} onClick={this.delete}>Delete Post</button>
+      </details>
+
+
       <br />
       </div>
     )
